e8bcc96d09ae73a79cc361b0e306cc3c29cf27ca
antonio <antonio.popa.dev@gmail.com>
2025-07-08 10:25:49 +0300

    Update

---
+++ src/main/java/com/asusoftware/feet_flow_api/auth/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.asusoftware.feet_flow_api.auth.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+@AllArgsConstructor
+public class TestController {
+
+    @RequestMapping
+    public String test() {
+        return "Test endpoint is working!";
+    }
+}
